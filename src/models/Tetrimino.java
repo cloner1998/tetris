b2ab7335b756bfc96db6d7ca8f86f934b6254f60
cloner1998 <sajjadyami77@gmail.com>
2024-12-30 11:24:00 +0330

    [implementation] implement isValid move

--- src/models/Tetrimino.java
+++ src/models/Tetrimino.java
@@ -2,14 +2,46 @@ package models;
 
 public class Tetrimino {
     private int[][] shape;
-    private int[][] coordinates;
+    private int x;
+    private int y;
     private int color;
 
-    public Tetrimino(int[][] shape, int[][] coordinates, int color) {
+    public Tetrimino(int[][] shape, int x, int y, int color) {
         this.shape = shape;
-        this.coordinates = coordinates;
+        this.x = x;
+        this.y = y;
         this.color = color;
     }
 
+    public void setShape(int[][] shape) {
+        this.shape = shape;
+    }
 
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void setColor(int color) {
+        this.color = color;
+    }
+
+    public int[][] getShape() {
+        return shape;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getColor() {
+        return color;
+    }
 }
--- src/serivces/BoardServices.java
+++ src/serivces/BoardServices.java
@@ -1,14 +1,44 @@
 package serivces;
 
+import models.Board;
 import models.Tetrimino;
 
 public class BoardServices {
+    public Board board;
+
+
 
     public boolean isValidMove(Tetrimino currentPiece, int deltaX, int deltaY) {
-        return false;
+        int[][] shape = currentPiece.getShape();
+        int[][] boardGrid = board.getGrid();  // Get the grid array
+
+        int newX = currentPiece.getX() + deltaX;
+        int newY = currentPiece.getY() + deltaY;
+
+        for(int row = 0;row < shape.length;row++){
+            for(int col = 0; col < shape[row].length; col++){
+                int gridX = newX + col;
+                int gridY = newY + row;
+                if (gridX < 0 || gridX >= board.getWidth() ||    // Left and right walls
+                        gridY < 0 || gridY >= board.getHeight()) {   // Top and bottom
+                    return false;
+                }
+                if (gridY >= 0 &&  boardGrid[gridX][gridY] != 0) {
+                    return false;
+                }
+            }
+
+        }
+
+        return true;
+    }
+    public void movePieceDown () {
     }
 
+    public void movePieceRight() {
+    }
 
-    public void movePieceDown() {
+    public void movePieceLeft() {
+
     }
 }
